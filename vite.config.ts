61fe73ccc9f5c99e4fb8793d176ec3eb426723bc
moises <moisaant@gmail.com>
2022-08-28 15:18:25 -0300

    fix lint warning - add vite.config in eslintignore

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: './setupTests.ts',
+    setupFiles: './src/setupTests.ts',
     include: [...configDefaults.include],
     exclude: [...configDefaults.exclude, "packages/template/*"]
   }
